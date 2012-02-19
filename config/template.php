7b9cb48d8feb62e8763484fb706799818dc5dfdb
Kusmayadi <kusmayadi@cobacobi.com>
2012-02-19 09:47:54 +0800

    Fixed bug on getting asset, removed Chrome & Safari from template configuration

--- config/template.php
+++ config/template.php
@@ -1,9 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	'Chrome'	=> array(
-		'Mac OS X'	=> array('15>' => 'webkit')
-	), 
-	'Safari'	=> 'webkit',
 	'*'			=> 'general'
 );
